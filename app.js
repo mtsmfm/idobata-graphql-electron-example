5c0fdd917e9ed60a5437fdaf79192c7260166d18
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2017-04-22 02:11:18 +0900

    Step 1: Display viewer's name

--- app.js
+++ app.js
@@ -1,7 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Relay from 'react-relay';
+
+Relay.injectNetworkLayer(
+  new Relay.DefaultNetworkLayer('https://api.idobata.io/graphql', {
+    headers: {
+      'Authorization': `Bearer ${process.env.IDOBATA_TOKEN}`
+    }
+  })
+)
+
+class AppRoute extends Relay.Route {
+  static queries = {
+    viewer: () => Relay.QL`
+      query {
+        viewer
+      }
+    `
+  };
+  static routeName = 'AppRoute';
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Hello @{this.props.viewer.name}!
+        </h1>
+      </div>
+    )
+  }
+}
+
+App = Relay.createContainer(App, {
+  fragments: {
+    viewer: () => Relay.QL`
+      fragment on Guy {
+        name
+      }
+    `,
+  },
+});
 
 ReactDOM.render(
-  <div>Hello from React</div>,
+  <Relay.RootContainer
+    Component={App}
+    route={new AppRoute()}
+  />,
   document.getElementById('root')
 );
